c6f16be59ce52002b1c2cf40d76e1f39a1be1843
Piero Dev <pierodev21@gmail.com>
2025-03-15 13:43:22 -0500

    feat: add coloscheme one dark

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,13 @@
+return {
+    -- Lazy
+    {
+        "olimorris/onedarkpro.nvim",
+        priority = 1000, -- Ensure it loads first
+    },
+    {
+        "LazyVim/LazyVim",
+        opts = {
+            colorscheme = "onedark",
+        },
+    },
+}
