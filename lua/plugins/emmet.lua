2463bfb9d9c3fae2d0592103baab07be84955c6b
Piero Dev <pierodev21@gmail.com>
2025-04-01 20:38:15 -0500

    feat(mason): add emmet-language-server

--- lua/plugins/emmet.lua
+++ lua/plugins/emmet.lua
@@ -1,8 +1,13 @@
 return {
     {
+        "williamboman/mason.nvim",
+        opts = { ensure_installed = { "emmet-language-server" } },
+      },
+    {
         "olrtg/nvim-emmet",
         config = function()
             vim.keymap.set({ "n", "v" }, '<leader>cw', require('nvim-emmet').wrap_with_abbreviation,{ desc = "Wrap tags" })
         end,
     },
+    
 }
