abcbc1dccc1de3cff802e14e5e42020a8fa83ef2
Piero Dev <pierodev21@gmail.com>
2025-04-01 20:40:41 -0500

    feat(cmp): add key tab to change suggestion

--- lua/plugins/supertab.lua
+++ lua/plugins/supertab.lua
@@ -5,18 +5,19 @@ return {
     opts = {
         keymap = {
             -- Tab key behavior:  
-            ["<Tab>"] = {
-                function(cmp)
-                    if cmp.snippet_active() then
-                        return cmp.accept()
-                    else
-                        return cmp.select_and_accept()
-                    end
-                end,
-                "snippet_forward",
-                "fallback",
-            },
+            -- ["<Tab>"] = {
+            --     function(cmp)
+            --         if cmp.snippet_active() then
+            --             return cmp.accept()
+            --         else
+            --             return cmp.select_and_accept()
+            --         end
+            --     end,
+            --     "snippet_forward",
+            --     "fallback",
+            -- },
 
+            ['<Tab>'] = { 'select_next', 'fallback_to_mappings' },
             -- Ctrl + K: Select the previous suggestion
             ['<C-k>'] = { 'select_prev', 'fallback_to_mappings' },
             -- Ctrl + J: Select the next suggestion
