c8de910b48cd60215c8317a66ea2b0bf2a0a2f63
Piero Dev <pierodev21@gmail.com>
2025-04-14 14:43:29 -0500

    chore(ui): add missing diagnostic disable comment in noice.nvim configuration

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -1,27 +1,27 @@
 return {
-    {
-        "folke/noice.nvim",
-        config = function()
-          require("noice").setup({
-            cmdline = {
-              view = "cmdline", -- Use the cmdline view for the command-line
-            },
-            presets = {
-              bottom_search = true, -- Enable bottom search view
-              command_palette = true, -- Enable command palette view
-              lsp_doc_border = true, -- Enable LSP documentation border
-            },
-            -- Uncomment the following lines to customize the cmdline popup view
-            -- views = {
-            --   cmdline_popup = {
-            --     filter_options = {},
-            --     win_options = {
-            --       winhighlight = "NormalFloat:NormalFloat,FloatBorder:FloatBorder",
-            --     },
-            --   },
-            -- },
-          })
-        end,
-      },
-    
+  {
+    "folke/noice.nvim",
+    config = function()
+      ---@diagnostic disable-next-line: missing-fields
+      require("noice").setup({
+        cmdline = {
+          view = "cmdline", -- Use the cmdline view for the command-line
+        },
+        presets = {
+          bottom_search = true, -- Enable bottom search view
+          command_palette = true, -- Enable command palette view
+          lsp_doc_border = true, -- Enable LSP documentation border
+        },
+        -- Uncomment the following lines to customize the cmdline popup view
+        -- views = {
+        --   cmdline_popup = {
+        --     filter_options = {},
+        --     win_options = {
+        --       winhighlight = "NormalFloat:NormalFloat,FloatBorder:FloatBorder",
+        --     },
+        --   },
+        -- },
+      })
+    end,
+  },
 }
