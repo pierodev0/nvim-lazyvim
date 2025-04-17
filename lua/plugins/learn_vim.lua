8cec2ee74a6c6a1b5bb536c6a34ce98dd2eb5539
Piero Dev <pierodev21@gmail.com>
2025-04-16 20:38:07 -0500

    feat(learn_vim): add hardtime.nvim for improved Vim learning

---
+++ lua/plugins/learn_vim.lua
@@ -0,0 +1,12 @@
+return {
+  {
+    "m4xshen/hardtime.nvim",
+    dependencies = { "MunifTanjim/nui.nvim" },
+    opts = {},
+  },
+  -- {
+  --   "tris203/precognition.nvim",
+  --   --event = "VeryLazy",
+  --   opts = {},
+  -- },
+}
