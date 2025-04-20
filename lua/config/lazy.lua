cab08d88eb2576bb35789294653c2b1530b544de
Piero Dev <pierodev21@gmail.com>
2025-04-20 14:21:35 -0500

    feat(markdown): add markdown-related plugins and configuration

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -33,7 +33,6 @@ require("lazy").setup({
 
     -- Language support plugins
     { import = "lazyvim.plugins.extras.lang.json" },
-    { import = "lazyvim.plugins.extras.lang.markdown" },
     { import = "lazyvim.plugins.extras.lang.typescript" },
     { import = "lazyvim.plugins.extras.lang.tailwind" },
 
