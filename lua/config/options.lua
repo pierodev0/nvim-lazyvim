c812de0659ac8d39592e3507f35a2c296c9c964d
Piero Dev <pierodev21@gmail.com>
2025-05-07 17:21:01 -0500

    feat: Add pwsh as default shell instead of cmd.exe on win

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -9,3 +9,18 @@ vim.opt.wrap = true
 -- Config for minute-ai
 -- vim.g.lazyvim_blink_main = true
 
+
+-- Configurar pwsl por defecto en windows
+-- Referencia: https://github.com/LazyVim/LazyVim/issues/2151#issuecomment-1888294994
+if jit.os == 'Windows' then
+  local win_sh = nil
+  if vim.fn.executable("pwsh") then
+    win_sh = "pwsh"
+  elseif vim.fn.executable("powershell") then
+    win_sh = "powershell"
+  end
+  if win_sh then
+    LazyVim.terminal.setup(win_sh)
+  end
+end
+
